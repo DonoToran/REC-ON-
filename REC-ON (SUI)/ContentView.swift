99f4da9346031920fe0466969f85a7f3bee991f8
DonoToran <donovan.toran@students.tamuk.edu>
2022-03-30 18:44:27 -0500

    Initial Commit

---
+++ REC-ON (SUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REC-ON (SUI)
+//
+//  Created by Miguel Tor on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ REC-ON (SUI)/REC_ON__SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  REC_ON__SUI_App.swift
+//  REC-ON (SUI)
+//
+//  Created by Miguel Tor on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct REC_ON__SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
